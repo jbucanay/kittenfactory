705cb7d85f8decafc1f32c485d359b24b70c88c1
Ashley Farr <98714342+ashleyfarr@users.noreply.github.com>
2022-02-27 20:45:33 -0700

    Uploading merged Stephen/Ashley work

---
+++ php_html/EmployeePortal.php
@@ -0,0 +1,28 @@
+<?php
+
+
+echo <<<_END
+	<head>
+		<title>Employee Portal</title>
+		<link rel='stylesheet' href='../css/formatting.css'>
+	</head>
+	<header>
+		<a href='HomePage.php'>
+			<img src='../images/logo.png' width= "200" height="200" />
+		</a>
+		<a href='AboutUs.php'><button> About Us </button></a>
+		<a href='Shop.php'><button> Shop </button></a>
+		<a href='Login.php'><button> Login </button></a>
+	</header>
+	<body>
+		<h1> Kitten Factory </h1>
+		<h2> Employee Portal </h2>
+		<br>
+		<button> Team and Org-Chart </button>
+		<button> Customer and Order Management </button>
+		<button> Raw Materials Management </button>
+	</body>
+_END;
+
+
+?>
