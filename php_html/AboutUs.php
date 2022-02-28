4719d817c1b18580bbcac0df9b8aeb9dc0372382
stephen_bigelow <bigelowis@gmail.com>
2022-02-27 17:30:04 -0700

    organized files into directories and formatted some of the php code

---
+++ php_html/AboutUs.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>About Us</title>
+		<link rel='stylesheet' href='../css/formatting.css'>
+	</head>
+		<header>
+			<a href='HomePage.php'>
+				<img src='../images/logo.png' width= "200" height="200" />
+			</a>
+			<a href='AboutUs.php'><button> About Us </button></a>
+			<a href='Shop.php'><button> Shop </button></a>
+			<a href='Login.php'><button> Login </button></a>
+		</header>
+		<body>
+			<h1> Kitten Factory </h1>
+			Kitten Factory is a local ski manufacturing company that specializes in customized carbon fiber skis. The owners and operators are local entrepreneurs with mechanical engineering degrees from the University of Utah with over 14 years of combined ski making experience. The products produced by Kitten Factory are made using carbon fiber which makes them lighter than other fiberglass skis found on the market. The lighter weight will help consumers have a more enjoyable ride no matter the terrain. In addition to providing top of the line products, the company is concerned with adding to the local economy.  They offer a wide variety of skis including, park, powder, and touring skis with hybrid wood and carbon lay ups to full carbon lay ups making their skis some of the most durables skis on the market. Over the past 6 years the owners, employees and team riders have been riding the skis they make every day in order to create the best possible product for the consumer that will withstand the test of time. Kitten Factory Skis are truly a rider owned and operated company looking to create the highest quality product available for the skier who is looking to go into the park and backcountry and really test their equipment for what it was meant for. The local winter recreation industry has always been a staple of the Utah market and Kitten Factory is slowly making a name for itself.
+			<br>
+		</body>
+		<footer>
+			<button> Employee Login </button>
+		</footer>
+</html>
+
+
+
+
+<?php
+# kittenfactory
+
+?>
---
+++ php_html/HomePage.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>Home</title>
+		<link rel='stylesheet' href='../css/formatting.css'>
+	</head>
+	<header>
+		<a href='HomePage.php'>
+			<img src='../images/logo.png' width= "200" height="200" />
+		</a>
+		<a href='AboutUs.php'><button> About Us </button></a>
+		<a href='Shop.php'><button> Shop </button></a>
+		<a href='Login.php'><button> Login </button></a>
+	</header>
+	<body>
+		<h1> Kitten Factory </h1>
+		<h2> Custom Carbon Fiber Skis </h2>
+	</body>
+	<footer>
+		<button> Employee Login </button>
+	</footer>
+</html>
+
+<?php
+# kittenfactory
+
+?>
+
