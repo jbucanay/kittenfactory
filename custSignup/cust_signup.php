95b46bd659001d96cd402ce4df4f993f0e9d007c
jbucanay <gilbert.baracka@gmail.com>
2022-03-11 00:26:17 -0700

    Get username

--- custSignup/cust_signup.php
+++ custSignup/cust_signup.php
@@ -27,31 +27,31 @@
         <div class="row mb-3">
           <label for="inputEmail3" class="col-sm-2 col-form-label">First Name</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="first_name">
           </div>
         </div>
         <div class="row mb-3">
           <label for="inputEmail3" class="col-sm-2 col-form-label">Last Name</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="last_name">
           </div>
         </div>
         <div class="row mb-3">
           <label for="inputPassword3" class="col-sm-2 col-form-label">Address</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" >
+            <input type="text" class="form-control" name="address">
           </div>
         </div>
         <div class="row mb-3">
           <label for="inputEmail3" class="col-sm-2 col-form-label">User Name</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="user_name">
           </div>
         </div>
         <div class="row mb-3">
           <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
           <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3">
+            <input type="password" class="form-control" id="inputPassword3" name='password'>
           </div>
         </div>
         <button type="submit" class="btn btn-primary" ><a target="_self" class='atag'>Submit</a></button>
--- custSignup/signup_cust.php
+++ custSignup/signup_cust.php
@@ -1,14 +1,48 @@
 <?php 
+
+//database login creditials
 $sn = "localhost:8889";
 $un = "root";
 $pw = "root";
 $db = "kitten_factory";
 
 try {
-    $conn = new PDO("mysql:host$sn;dbname=$db", $un,$pw);
+
+    // Connection to database
+    // source: https://www.w3schools.com/php/php_mysql_connect.asp 
+    $conn = new PDO("mysql:host=$sn;dbname=$db", $un,$pw);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "Connected Successfully";
     
+    echo "<br>";
+    //get values from form
+    if(!empty(($_POST['first_name'])) 
+        && !empty(($_POST['first_name']))
+        && !empty(($_POST['last_name']))
+        && !empty(($_POST['address']))
+        && !empty(($_POST['user_name']))
+        && !empty(($_POST['password']))
+        )
+    {
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $address = $_POST['address'];
+        $user_name = $_POST['user_name'];
+        $password = $_POST['password'];
+        
+        $qr = "SELECT * FROM customer WHERE user_name = '$user_name'";
+        $res = $conn->query($qr);
+        print_r($res);
+
+
+       
+        
+        
+    }
+    else {
+        echo 'not complete';
+    }
+
+
 
 
 } catch (PDOException $e){
