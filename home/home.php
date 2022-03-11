48a15d559aedab5cb2918a93379d90400a193ddc
jbucanay <gilbert.baracka@gmail.com>
2022-03-10 20:14:51 -0700

    ashley changes

--- home/home.php
+++ home/home.php
@@ -16,16 +16,10 @@
     </style>
 <title>Kitten Factory | Ski Manufacturing</title>
 </head>
-<<<<<<< HEAD
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<div class="container">
-    <a class="navbar-brand" href="#">
-=======
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <div class="container">
     <a class="navbar-brand" href="../home/home.php">
->>>>>>> ashley
       <img src="../images/logo.png" alt="" width="50" height="50">
     </a>
   </div>
@@ -36,28 +30,6 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
       <li class="nav-item">
-<<<<<<< HEAD
-          <a class="nav-link" aria-current="page" href="#">Skis</a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">About</a>
-        </li>
-        
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../login/login.php">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-            </svg></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../login/login.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-=======
           <a class="nav-link" aria-current="page" href="../shop/shop.php">Skis</a>
         </li>
 
@@ -71,7 +43,6 @@
         
         <li class="nav-item">
           <a class="nav-link" href="../viewcart/viewcart.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
->>>>>>> ashley
           <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg></a>
         </li>
@@ -80,8 +51,6 @@
   </div>
 </nav>
 
-<<<<<<< HEAD
-=======
 <body>
 
 	<h1> Kitten Factory </h1>
@@ -90,7 +59,6 @@
 
 </body>
 	
->>>>>>> ashley
 
 
 </html>
