d3e3c5548beef17c6e7b777dd8cc3b12e3c5bbe3
jbucanay <gilbert.baracka@gmail.com>
2022-03-10 23:26:11 -0700

    connectiong

--- custSignup/signup_cust.php
+++ custSignup/signup_cust.php
@@ -8,8 +8,13 @@ try {
     $conn = new PDO("mysql:host$sn;dbname=$db", $un,$pw);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo "Connected Successfully";
+    
+
+
 } catch (PDOException $e){
     echo "Connection failed: " .$e->getMessage();
 }
+
+
 
 ?>
