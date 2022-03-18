9f66b79ee497645648fdf69f8165804b19369f4e
stephen_bigelow <bigelowis@gmail.com>
2022-03-18 14:10:12 -0600

    fixed foreign key error on delete cust

--- customer_management/process_delete_cust.php
+++ customer_management/process_delete_cust.php
@@ -10,12 +10,22 @@ if(isset($_POST['customer_id'])) {
 	
 	$customer_id = $_POST['customer_id'];
 
-	$query = "delete from customer where customer_id = $customer_id";
+	$delete_cust_orders = "delete from order_table where customer_id = $customer_id";
+	
+	$delete_cust_orders_result = $conn->query($delete_cust_orders);
+	if(!$delete_cust_orders_result) die($conn->error);
+}
 
-	$result = $conn->query($query);
-	if(!$result) die($conn->error);
+if(isset($delete_cust_orders)) {
+
+	$delete_cust = "delete from customer where customer_id = $customer_id";
+
+	$delete_cust_result = $conn->query($delete_cust);
+	if(!$delete_cust_result) die($conn->error);
+
+
+}
 
 	header("Location: customer_management.php");
-}
 
 ?>
